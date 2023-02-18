6e4de0fd2238f4e8c2234f82367666b92ba4f55f
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2023-02-18 16:56:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from rectangle import *
+   
+class parallelepiped(rectangle):
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.h = height
+        
+    def volume(self):
+        volume = self.l*self.b*self.h
+        print("Volume =", volume)
+    
+
+r = rectangle(5, 6)
+r.display()
+
+r1 = parallelepiped(2, 3, 4)
+r1.display()
+r1.volume()
