a85d24d96a99b43953a9e2229f4cc12a91a9cef4
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2023-02-18 16:54:41 +0530

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,20 @@
+#problem 8
+class rectangle:
+    def __init__(self, length, width):
+        self.l = length
+        self.b = width
+        
+    def perimeter(self):
+        perimeter = 2*(self.l + self.b)
+        return perimeter
+        
+    def area(self):
+        area = self.l * self.b
+        return area
+        
+    def display(self):
+        print("Length and Width of rectangle:", self.l, self.b)
+        print("Perimeter =", self.perimeter())
+        print("Area =", self.area())
+    
+        
